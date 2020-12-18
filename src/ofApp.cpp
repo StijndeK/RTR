1e8a668c7674779137358f6902bf646c10838d99
Stijn de Koning <sdk.stijn@gmail.com>
2020-12-18 10:38:24 +0100

    FMOD integration

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,79 @@
 #include "ofApp.h"
 
+/* TODO:
+* - seperate class for audio handling
+* - function for initialise, play, stop, set volume
+* - try without the plugin in Unreal
+*/
+
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+	test.load("shoplifters.jpg");
+	audio.setup();
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+	audio.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	test.draw(0, 0);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+	std::cout << "key pressed" << std::endl;
+	audio.play();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
